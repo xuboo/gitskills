90f6a9dce95752a9a966452cfb06c17ae0a9c3f1
wayne <wayne>
2016-12-24 14:34:45 +0800

    初始化projection

---
+++ app/src/main/java/day01/itcast/com/sourcetree_test/CommonAdapter.java
@@ -0,0 +1,31 @@
+package day01.itcast.com.sourcetree_test;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by me2 on 2016/12/24.
+ */
+
+public class CommonAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
---
+++ app/src/main/java/day01/itcast/com/sourcetree_test/MainActivity.java
@@ -0,0 +1,13 @@
+package day01.itcast.com.sourcetree_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
